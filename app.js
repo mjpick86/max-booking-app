a608ec5930141605248fc38877854cd872f6acc9
Max Pick <mjpick86@gmail.com>
2026-02-17 15:45:21 +1030

    Add database

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const squareButton = document.getElementById('squareButton');
 const resultNumber = document.getElementById('resultNumber');
 
 squareButton.addEventListener('click', async () => {
+    
     const number = numberInput.value;
 
     if (!number) {
         return;
     }
     resultNumber.textContent = `Calculating...`;
-    const response = await fetch('https://max-booking-app.onrender.com/square', {
+    await fetch('http://localhost:8000/square', {
         method: 'POST',
         headers: {
             "Content-Type": "application/json",
@@ -17,6 +18,8 @@ squareButton.addEventListener('click', async () => {
         body: JSON.stringify({number: parseInt(number)}),
     });
 
+    const response = await fetch('http://localhost:8000/last_squares');
     const data = await response.json();
     resultNumber.textContent = `Result: ${data.result}`;
+
 });
--- backend/api/main.py
+++ backend/api/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+import psycopg2
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
+load_dotenv()  # Load environment variables from .env file
+db_password = os.environ.get('DB_PASSWORD', 'default_password')  # Use a default value if the environment variable is not set
 
 origins = [
     "http://127.0.0.1:5500",
     "https://mjpick86.github.io",
 ]
+
+conn = psycopg2.connect(
+    dbname="test_db",
+    user="postgres",
+    password=db_password,
+    host="localhost",
+    port="5432"
+)
 
 class Num(BaseModel):
     number: int
@@ -24,4 +37,20 @@ def health():
 
 @app.post("/square")
 def square(number: Num):
-    return {"result": number.number ** 2}
+    result = number.number ** 2
+    cur = conn.cursor()
+    cur.execute("INSERT INTO squares (squared) VALUES (%s);", (result,))
+    conn.commit()
+    return {"result": result}
+
+@app.get("/db_test")
+def db_test():
+    cur = conn.cursor()
+    cur.execute("SELECT version();")
+    return {"version": cur.fetchone()}
+
+@app.get("/last_squares")
+def last_squares():
+    cur = conn.cursor()
+    cur.execute("SELECT squared FROM squares ORDER BY id DESC LIMIT 5;")
+    return {"result": [row[0] for row in cur.fetchall()]}
